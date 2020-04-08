7c4f2877aa690d5b5f28131bc1ade28b3ea32229
Calo Corp <marc.calo@codeimmersives.com>
2020-04-08 16:03:10 -0400

    WIP

--- main.js
+++ main.js
@@ -1,6 +1,34 @@
-const Ray = () => {
+const Ray = function () {
+ return  {
+    
+    length: 0,
+    push: function (value){
+    this[this.length++] = value; 
+
+    },
+    pop: function (){
+    const popped = this[this.length -1]
+
+     delete this[this.length-1]
+     this.length--
+      return popped 
+    },
+    unshift: function(){
+    
+      this.length++
+    },
+    shift: function (){
+      const shifted= this[0]
+      
+      
+      return shifted
+    }
+  }
   
+ 
+
 }
+
 
 module.exports = {
   Ray,
